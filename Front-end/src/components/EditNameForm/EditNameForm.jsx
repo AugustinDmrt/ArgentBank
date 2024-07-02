67914bd8cb609c1138c0681f46be94858ff718d2
AugustinDmrt <55685689+AugustinDmrt@users.noreply.github.com>
2024-07-02 19:21:23 +0200

    get > post dans les swaqger

--- Front-end/src/components/EditNameForm/EditNameForm.jsx
+++ Front-end/src/components/EditNameForm/EditNameForm.jsx
@@ -6,8 +6,8 @@ import "./EditNameForm.sass";
 const EditNameForm = () => {
   const dispatch = useDispatch();
   const name = useSelector((state) => state.profile.name);
-  const token = useSelector((state) => state.auth.token); // Assurez-vous de récupérer le jeton
-  const userId = useSelector((state) => state.auth.userId); // Assurez-vous de récupérer l'ID utilisateur
+  const token = useSelector((state) => state.auth.token);
+  const userId = useSelector((state) => state.auth.userId);
   const [newName, setNewName] = useState(name);
 
   const handleChange = (e) => {
